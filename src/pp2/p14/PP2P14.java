1874397fa6e0f0b09bd929d123a481f6209d44bb
Osvaldo <Osvaldo@Osvaldo>
2016-10-17 17:57:44 -0500

    7x7

--- src/pp2/p14/PP2P14.java
+++ src/pp2/p14/PP2P14.java
@@ -19,19 +19,22 @@ public class PP2P14 {
         int[][] arreglo = new int[7][7];
         llenarArreglo(arreglo);
     }
-    //solo un metodo void
+    //solo un metodo void para todo el programa
     public static void llenarArreglo(int [][]a){
         int i,j,p=0;
         for(i=0;i<7;i++){
               for(j=0;j<7;j++){
                   if(i==j){
+                      //solo se escribira un 1 cuando i y j son iguales
                       p=1;
                   }else{
                       p=0;
                   }
                   System.out.print(p +"\t");
+                  //Usando el comando "\t" para tabular
             }
               System.out.println("\n");
+              //Aqui va el salto de lineas porque salta cada vez que evalua j
         }
         
     }
